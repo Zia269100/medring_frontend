0d3d20093dd10ff090398dca8239fe49dfd6d55c
Zia269100 <md.ziakhalid2691@gmail.com>
2026-02-08 15:35:41 +0530

    sfg

---
+++ src/pages/AdminLogin.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import api from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+export default function AdminLogin() {
+
+  const nav = useNavigate();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  });
+
+  const login = async () => {
+    const res = await api.post("/admin/login", form);
+    localStorage.setItem("adminToken", res.data.token);
+    nav("/admin");
+  };
+
+  return (
+    <div className="card">
+      <h2>Admin Login</h2>
+
+      <input
+        placeholder="Email"
+        onChange={(e)=>setForm({...form,email:e.target.value})}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e)=>setForm({...form,password:e.target.value})}
+      />
+
+      <button onClick={login}>
+        Login
+      </button>
+    </div>
+  );
+}
