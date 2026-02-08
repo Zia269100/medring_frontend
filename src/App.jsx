fe0e7b23f2b9b76168c7011503de3613c35cc2f5
Zia269100 <md.ziakhalid2691@gmail.com>
2026-02-08 16:25:45 +0530

    rg

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import OwnerDashboard from "./pages/OwnerDashboard";
-import EmergencyView from "./pages/EmergencyView";
-import DoctorDashboard from "./pages/DoctorDashboard";
-import DoctorRegister from "./pages/DoctorRegister";
-import DoctorLogin from "./pages/DoctorLogin";
-import AdminPanel from "./pages/AdminPanel";
-import AdminAnalytics from "./pages/AdminAnalytics";
-import AdminLogin from "./pages/AdminLogin";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-
-      <Routes>
-
-        {/* home */}
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-
-        {/* ring */}
-        <Route path="/r/:token" element={<EmergencyView />} />
-        <Route path="/owner/:token" element={<OwnerDashboard />} />
-
-        {/* doctor */}
-        <Route path="/doctor/login" element={<DoctorLogin />} />
-        <Route path="/doctor/register" element={<DoctorRegister />} />
-        <Route path="/doctor" element={<DoctorDashboard />} />
-
-        {/* admin */}
-        <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/analytics" element={<AdminAnalytics />} />
-
-      </Routes>
-    </BrowserRouter>
-  );
+  return <h1>WORKING</h1>;
 }
