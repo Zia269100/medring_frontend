b01465832a9707d92e2676a6945396a732a5342f
Zia269100 <md.ziakhalid2691@gmail.com>
2026-02-08 16:16:07 +0530

    db

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 import api from "../services/api";
 import { useNavigate } from "react-router-dom";
--- src/pages/EmergencyView.jsx
+++ src/pages/EmergencyView.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
